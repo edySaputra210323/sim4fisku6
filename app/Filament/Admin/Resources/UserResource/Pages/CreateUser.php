e2117f2c8833b65612d381fc0853919303487a80
meeftah <miftah.yuda@gmail.com>
2024-10-01 10:03:05 +0700

    fix: set redirect to index and modify mutate form data

--- app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
@@ -9,4 +9,20 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (auth()->user()->hasRole(['superadmin'])) {
+            $data['email_verified_at'] = now();
+        }
+
+        $data['username'] = \Str::slug($data['username']);
+
+        return $data;
+    }
 }
--- app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
@@ -10,10 +10,45 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    public function mount(int | string $record): void
+    {
+        if (!auth()->user()->hasRole('superadmin') && $this->record == 1) {
+            abort(403);
+        }
+        $this->record = $this->resolveRecord($record);
+
+        $this->authorizeAccess();
+
+        $this->fillForm();
+
+        $this->previousUrl = url()->previous();
+    }
+
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (!$data['password']) {
+            unset($data['password']);
+        }
+
+        if (auth()->user()->hasRole(['superadmin'])) {
+            $data['email_verified_at'] = now();
+        }
+
+        $data['username'] = \Str::slug($data['username']);
+
+        return $data;
     }
 }
