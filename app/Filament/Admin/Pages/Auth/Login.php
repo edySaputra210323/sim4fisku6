b6652a35f2431ffb21e6e25d83246ff4b5621e6a
meeftah <miftah.yuda@gmail.com>
2024-10-01 10:01:48 +0700

    feat: add login

---
+++ app/Filament/Admin/Pages/Auth/Login.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Admin\Pages\Auth;
+
+use Filament\Forms;
+use Filament\Pages\Auth\Login as BaseAuth;
+use Illuminate\Validation\ValidationException;
+
+class Login extends BaseAuth
+{
+    public function form(Forms\Form $form): Forms\Form
+    {
+        return $form
+            ->schema([
+                $this->getLoginFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getLoginFormComponent(): Forms\Components\Component
+    {
+        return Forms\Components\TextInput::make('login')
+            ->label('Login')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+
+        return [
+            $login_type => $data['login'],
+            'password'  => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
+    }
+}
